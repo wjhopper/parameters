69a9c2ae7d012918ec414eb9be0e86e5a9cac7cb
whopper-sculder <wjhopper510@gmail.com>
2016-10-17 19:21:06 -0400

    Added validity checking method for parameter objects that makes sure supplied value is between the value of upper_bound and lower_bound slots

--- R/parameters.R
+++ R/parameters.R
@@ -6,4 +6,12 @@ setClass(Class = "parameter",
          prototype = list(value = NA_real_,
                           name = NA_character_,
                           upper_bound = Inf,
-                          lower_bound = -Inf))
+                          lower_bound = -Inf),
+         validity = function(object) {
+           if (object@value <= object@upper_bound && object@value >= object@lower_bound) {
+             return(TRUE)
+           } else {
+             return(paste("Supplied parameter value", object@value, "is not between",
+                          object@lower_bound, "and", object@upper_bound))
+           }
+         })
