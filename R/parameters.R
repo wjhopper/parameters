a677f50d0869483e9a5e213457c5ccf3157174b7
whopper-sculder <wjhopper510@gmail.com>
2016-10-18 14:14:33 -0400

    Added function and class documentation

--- R/parameters.R
+++ R/parameters.R
@@ -1,3 +1,21 @@
+#' @title Bounded parameter objects
+#'
+#' @description An S4 class to represent a bounded parameter for a model
+#'
+#' @details No constructor function exists for \code{parameter} objects. Parameter
+#' objects should be instantiated with the \code{new()} function directly.
+#'
+#' @slot value A numeric vector of length one, which acts as the parameters value
+#' @slot name A character vector of length one, which acts as a semantic label
+#'   for what the parameter value represents
+#' @slot upper_bound A numeric vector of length one. The \code{value} slot may
+#'   not be assinged a value greater than the value of the \code{upper_bound} slot.
+#' @slot lower_bound A numeric vector of length one. The \code{value} slot may
+#'   not be assinged a value less than the value of the \code{lower_bound} slot.
+#'
+#' @examples
+#' new("parameter", value = .5, name = "binomial_p",
+#'     upper_bound = 1, lower_bound = 0)
 setClass(Class = "parameter",
          slots = list(value = "numeric",
                       name = "character",
@@ -16,10 +34,27 @@ setClass(Class = "parameter",
            }
          })
 
+#' @name value
+#' @title value
+#' @description Extract or replact the value of parameter object
+NULL
+
+#' @rdname value
+#' @param object A \code{parameter} object
+#'
+#' @export
+#' @examples
+#' p <- new("parameter", value = .5, name = "binomial_p",
+#'          upper_bound = 1, lower_bound = 0)
+#' value(p)
+#' value(p) <- .9
+#' value(p)
 setGeneric(name = "value",
            def = function(object) standardGeneric("value")
            )
 
+#' @describeIn value Returns the value held the in the \code{value} slot as a
+#' named numeric vector.
 setMethod(f = "value",
           signature = "parameter",
           definition = function(object) {
@@ -29,10 +64,18 @@ setMethod(f = "value",
           }
           )
 
+#' @rdname value
+#'
+#' @param value A numeric vector of length one. Must be fall between the parameter
+#' objects upper and lower bounds.
+#'
+#' @export
 setGeneric(name = "value<-",
            def = function(object, value) standardGeneric("value<-")
            )
 
+#' @describeIn value Replaces the value held the in the \code{value} slot
+#' @importFrom methods validObject
 setMethod(f = "value<-",
           signature = "parameter",
           definition = function(object, value) {
